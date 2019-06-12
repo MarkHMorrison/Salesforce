439c66d5ecfe3f27b487b8f3d752cd8d679d41d2
Mark H. Morrison <markhmorrison@gmail.com>
2019-06-12 16:53:23 -0500

    Clean comments

--- SOQL/Learn to Leverage the Power of SOQL.sql
+++ SOQL/Learn to Leverage the Power of SOQL.sql
@@ -1,72 +1,72 @@
-// Base
+-- Base
 SELECT Id, Name FROM Account
 
-// WHERE
+-- WHERE
 SELECT Id, Name From Account
 WHERE Phone = null
 
-// AND
+-- AND
 SELECT Id, Name From Account
 WHERE Phone = null
   AND Type = ‘Prospect’
 
-// OR
+-- OR
 SELECT Id, Name From Account
 WHERE Phone = null
   AND Type = ‘Prospect’
   AND (Industry = ‘Energy’ OR BillingState = ‘CA’)
 
-// LIMIT
+-- LIMIT
 SELECT Id, Name FROM Account
 LIMIT 250
 
-// ORDER BY (ASC / DESC)
+-- ORDER BY (ASC / DESC)
 SELECT Id, Name FROM Account
 ORDER BY Name ASC
 
-// IN ()
+-- IN ()
 SELECT Id, Name FROM Account
 WHERE BillingState IN (‘TN’,’GA’,’NC’)
 
-// NOT IN ()
+-- NOT IN ()
 SELECT Id, Name FROM Account
 WHERE BillingState NOT IN (‘TN’,’GA’,’NC’)
 
-// LIKE (% / _)
+-- LIKE (% / _)
 SELECT Id, Name FROM Contact
 WHERE  Email LIKE ‘%@g_ail.com’
 
-// COUNT()
+-- COUNT()
 SELECT COUNT() FROM Account
 WHERE Phone != null
 
-// GROUP BY
+-- GROUP BY
 SELECT BillingState, COUNT(Id) FROM Account
 GROUP BY  BillingState
 
-// SUM(), MIN(), MAX(), AVG()
+-- SUM(), MIN(), MAX(), AVG()
 SELECT SUM(Amount), MIN(Amount), MAX(Amount), AVG(Amount) FROM Opportunity
 
-// Querying a Parent Object Using Dot Notation
+-- Querying a Parent Object Using Dot Notation
 SELECT Id, Name, Account.Name
 FROM Contact
 
-// Querying a Related Object Using Dot Notation
+-- Querying a Related Object Using Dot Notation
 SELECT Invoice__c, Merchandise__r.Name
 FROM Line_Item__c
 
-// Querying a Child Object Using Sub Queries
+-- Querying a Child Object Using Sub Queries
 SELECT Id, Name, 
   (SELECT Name, Title FROM Contacts)
 FROM Account
 
-// Querying a Child Object Using Sub Queries
+-- Querying a Child Object Using Sub Queries
 SELECT Id, Name, Owner.Name
   (SELECT Id, Merchandise__r.Name 
    FROM Line_Items__r)
 FROM Invoice__c
 
-// Id field Semi-Join
+-- Id field Semi-Join
 SELECT Id, Name FROM Account
 WHERE Id IN 
   ( 
@@ -74,7 +74,7 @@ WHERE Id IN
     WHERE StageName = ‘Closed Lost’
   )
 
-// Reference Field Anti-Join
+-- Reference Field Anti-Join
 SELECT Id FROM Opportunity
 WHERE AccountId NOT IN 
 	( 
